2bad96ed94948313a0c4d0b584f45daeaa03689f
bragonya1992 <brayan.yaquian@fox.com>
2017-10-13 15:11:18 -0600

    first commit

---
+++ example/android/app/src/main/java/com/videoplayer/PackagesToast.java
@@ -0,0 +1,38 @@
+package com.videoplayer;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.List;
+
+/**
+ * Created by brayany on 10/13/2017.
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class PackagesToast implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new ToastModule(reactContext));
+
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
